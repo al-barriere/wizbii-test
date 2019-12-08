6baa0a7c106aa7ab719ebe04d96ddea7ba07fb23
al-barriere <alb.barriere@gmail.com>
2019-12-08 23:24:12 +0100

    solution POST et fix sur certains comportement

--- README.md
+++ README.md
@@ -14,6 +14,8 @@
 * `composer install` afin d'installer les bundles utilisés pour ce projet
 * création d'un fichier .env pour mettre les informations de sa base de données
 
-Exemple :
+Exemples :
 
-` curl -X GET 'http://127.0.0.1:8000/collect?t=pageview&dl=http://www.wizbii.com/bar&ec=ads&el=client&ea=Click_Masthead&ds=web&cn=bpce&cs=wizbii&cm=web&ck=erasmus&cc=foobar&v=1&tid=ER-4564-R&dr=http://www.azerty.com&wct=profile&wui=bariere-antoine&sn=screen_yo&an=application_name&av=2.4.17'`
+* ` curl -X GET 'http://127.0.0.1:8000/collect?t=pageview&dl=http://www.wizbii.com/bar&ec=ads&el=client&ea=Click_Masthead&ds=web&cn=bpce&cs=wizbii&cm=web&ck=erasmus&cc=foobar&v=1&tid=ER-4564-R&dr=http://www.azerty.com&wct=profile&wui=bariere-antoine&sn=screen_yo&an=application_name&av=2.4.17'`
+* `curl -X POST -d'[{"t":"event","dl":"company/wizbii","dr":"job/dev-backend-chez-wizbii","wct":"profile","wui":"remi-alvado","ec":"navigation","ea":"tap","el":"button-top","ev":1,"tid":"UA-1345-Y","ds":"apps","sn":"company","an":"WizbiiStudentAndroid","av":"1.2.1","qt":"123","v":"1"}]' 'http://127.0.0.1:8000/collect'
+   ["1","event","company\/wizbii","job\/dev-backend-chez-wizbii","profile","remi-alvado","navigation","tap","button-top",1,1,"apps","","","","","","company","WizbiiStudentAndroid","1.2.1"]`
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -24,7 +24,7 @@ class HomeController extends AbstractController
      */
     public function getCollect(Request $request,DocumentManager $dm){
 
-        $val = $this->checkAllParameter($request,$dm);
+        $val = $this->checkAllGetParameter($request,$dm);
 
         return $val;
     }
@@ -37,11 +37,237 @@ class HomeController extends AbstractController
      */
     public function postCollect(Request $request,DocumentManager $dm){
 
+        $content = json_decode($request->getContent());
+        $response = "";
+        // array_key_exists("v",$array_content)
+        for($i = 0; $i< sizeof($content); $i++){
+            $array_content = (array) $content[$i];
+            $response = $this->checkAllPostParameter($array_content,$dm);
+        }
+
+
+        return $response;
+    }
+
+    public function checkAllPostParameter($element, $dm){
+        $array = array();
+        $version = "";
+        $hitType = "";
+        $documentLocation = "";
+        $documentReferer = "";
+        $wizbiiCreatorType = "";
+        $wizbiiUserId = "";
+        $eventCategory = "";
+        $eventAction = "";
+        $eventLabel = "";
+        $eventValue = "";
+        $trackingId = "";
+        $dataSource = "";
+        $campaignName = "";
+        $campaignSource = "";
+        $campaignMedium = "";
+        $campaignKeyword = "";
+        $campaignContent = "";
+        $screenName = "";
+        $applicationName = "";
+        $applicationVersion = "";
+
+        // version
+        if((array_key_exists("v",$element)== false) ||
+            $element["v"] !== "1")
+        {
+            return new JsonResponse(array('Status' => '400 : Parameter v must be equal to 1 or exist'));
+        }
+        $version =   $element["v"];
+
+        // hit type
+        if ((array_key_exists("t",$element)== false) ||
+            !in_array($element["t"], array("pageview", "screenview", "event")))
+        {
+            return new JsonResponse(array('Status' => '400 : Parameter t must be equal to pageview, screenview, event or exist'));
+        }
+        $hitType = $element["t"];
+
+
+        // document location
+        if((array_key_exists("dl",$element)))
+        {
+            /*if($this->is_url($element["dl"]) === false)
+            {
+                return new JsonResponse(array('Status' => '400 : the document location must have a valid URL'));
+            }*/
+            $documentLocation = $element["dl"];
+        }
+
+        // document referer
+        if((array_key_exists("dl",$element)))
+        {
+            /*if($this->is_url($query->get('dr')) === false)
+            {
+                return new JsonResponse(array('Status' => '400 : the document referer must have a valid URL'));
+            }*/
+            $documentReferer = $element["dr"];
+        }
+
+        if($hitType == "screenview")
+        {
+
+            // wizbii creator type
+            if ((array_key_exists("wct",$element)== false) ||
+                !in_array($element['wct'], array("profile", "recruiter", "visitor", "wizbii_employee")))
+            {
+                return new JsonResponse(array('Status' => '400 : Parameter wct must be equal to profile, recruiter, visitor, wizbii_employee  or exist'));
+            }
+            $wizbiiCreatorType = $element["wct"];
+
+            // wizbii user id
+            if(array_key_exists("wui",$element)== false)
+            {
+                return new JsonResponse(array('Status' => '400 : Parameter wui must exist'));
+            }else{
+                $regex = '/^[aA-zZ]+-[aA-zZ]+$/';
+                if(!preg_match($regex,$element["wui"])){
+                    return new JsonResponse(array('Status' => '400 : Parameter wui must respect format slug firstname-lastname'));
+                }
+            }
+            $wizbiiUserId =$element["wct"];
+
+            // screen name
+            if(array_key_exists("sn",$element)== false)
+            {
+                return new JsonResponse(array('Status' => '400 : Parameter sn must exist'));
+            }
+            $screenName = $element["sn"];
+        }else
+        {
+            // wizbii creator type
+            if (array_key_exists("wct",$element) &&
+                !in_array($element["wct"],array("profile", "recruiter", "visitor", "wizbii_employee")))
+            {
+                return new JsonResponse(array('Status' => '400 : Parameter wct must be equal to profile, recruiter, visitor, wizbii_employee  or exist'));
+            }
+            $wizbiiCreatorType =  array_key_exists("wct",$element)? $element["wct"]: "";
+
+            // wizbii user id
+            $regex = '/^[aA-zZ]+-[aA-zZ]+$/';
+            if(array_key_exists("wui",$element) &&
+                (!preg_match($regex, $element["wui"]))){
+                return new JsonResponse(array('Status' => '400 : Parameter wui must respect format slug firstname-lastname'));
+            }
+            $wizbiiUserId = array_key_exists("wui",$element)? $element["wui"]: "";
+
+            $screenName = array_key_exists("sn",$element)? $element["sn"]: "";
+        }
+
+        if($hitType == "event")
+        {
+            // event category
+            if((array_key_exists("ec",$element)== false) || (strlen( $element["ec"])==0))
+            {
+                return new JsonResponse(array('Status' => '400 : Parameter ec must exist and not be empty'));
+            }
+            $eventCategory =  $element["ec"];
+
+            // event action
+            if((array_key_exists("ea",$element)== false) || (strlen( $element["ea"])==0))
+            {
+                return new JsonResponse(array('Status' => '400 : Parameter ea must exist and not be empty'));
+            }
+            $eventAction =  $element["ea"];
+        }else{
+            // event category
+            if((array_key_exists("ec",$element)== false) || (strlen($element["ec"])==0))
+            {
+                return new JsonResponse(array('Status' => '400 : Parameter ec must exist and not be empty'));
+            }
+            $eventCategory = array_key_exists("ec",$element)?$element["ec"]:"";
+
+            // event action
+            if((array_key_exists("ea",$element)== false) || (strlen($element["ea"])==0))
+            {
+                return new JsonResponse(array('Status' => '400 : Parameter ea must exist and not be empty'));
+            }
+            $eventAction = array_key_exists("ea",$element)?$element["ea"]:"";
+        }
+
+        // event label
+        if(array_key_exists("el",$element)){
+            $eventLabel = $element["el"];
+        }
+
+        // event value
+        if(array_key_exists("ev",$element) && !is_numeric($element["ev"]))
+        {
+            return new JsonResponse(array('Status' => '400 : Parameter ev must be a number'));
+        }else {
+            if($element["ev"]< 0)
+            {
+                return new JsonResponse(array('Status' => '400 : Parameter ev must be a positive number'));
+            }
+            $eventValue = $element["ev"];
+        }
+
+        // tracking id
+        $regex = '/^[A-Z0-9]{2}-[A-Z0-9]{4}-[A-Z0-9]{1}$/';
+       if((array_key_exists("tid",$element) == false) ||
+            (!preg_match($regex, $element["tid"])))
+        {
+            return new JsonResponse(array('Status' => '400 : Parameter tid  must exist or respect format UA-XXXX-Y',));
+        }
+        $trackingId = $element["ev"];
+
+        //data source
+        if ((array_key_exists("ds",$element)== false) ||
+            !in_array($element["ds"], array("web", "apps", "backend")))
+        {
+            return new JsonResponse(array('Status' => '400 : Parameter ds must be equal to web, apps, backend or exist'));
+        }
+        $dataSource = $element["ds"];
+
+        // campaign name
+        if(array_key_exists("cn",$element))
+            $campaignName = $element["cn"];
+
+        // campaign source
+        if(array_key_exists("cs",$element))
+            $campaignSource = $element["cs"];
+
+        // campaign medium
+        if(array_key_exists("cm",$element))
+            $campaignMedium = $element["cm"];
+
+        // campaign keyword
+        if(array_key_exists("ck",$element))
+            $campaignKeyword = $element["ck"];
+
+        // campaign content
+        if(array_key_exists("cc",$element))
+            $campaignContent = $element["cc"];
+
+        // application name
+        if($dataSource == "web"){
+            if(array_key_exists("an",$element) == false){
+                return new JsonResponse(array('Status' => '400 : Parameter an must exist if data source equals to web'));
+            }
+            $applicationName = $element["an"];
+        }else{
+            $applicationName = array_key_exists("an",$element)? $element["an"]: "";
+        }
+
+        // application version
+        $regex = '/^[0-9]+(.[0-9]+)*$/';
+        if(array_key_exists("av",$element) && !preg_match($regex, $element["av"])){
+            return new JsonResponse(array('Status' => '400 : Parameter av must respect format version'));
+        }
+        $applicationVersion = array_key_exists("av",$element)? $element["av"] : "";
+
+        array_push($array,$version,$hitType,$documentLocation,$documentReferer,$wizbiiCreatorType,$wizbiiUserId,$eventCategory,$eventAction,$eventLabel,$eventValue,$trackingId,$dataSource,$campaignName,$campaignSource,$campaignMedium,$campaignKeyword,$campaignContent,$screenName,$applicationName,$applicationVersion);
+        $this->createElemDb($array,$dm);
+        return new JsonResponse($array,200);
 
     }
 
-
-    public function checkAllParameter(Request $request, $dm){
+    public function checkAllGetParameter(Request $request, $dm){
         $query = $request->query;
         $array = array();
         $version = "";
@@ -234,12 +460,12 @@ class HomeController extends AbstractController
             $campaignKeyword = $query->get('ck');
 
         // campaign content
-        if($query->has('cn'))
-            $campaignContent = $query->get('cn');
+        if($query->has('cc'))
+            $campaignContent = $query->get('cc');
 
         // application name
         if($dataSource == "web"){
-            if($query->get('an') == false){
+            if($query->has('an') == false){
                 return new JsonResponse(array('Status' => '400 : Parameter an must exist if data source equals to web'));
             }
             $applicationName = $query->get('an');
